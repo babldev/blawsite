33b359c62e94ab1bf6abbaf8c76363b97f37c160
Brady Law <bradyl16@gmail.com>
2015-09-07 12:27:55 -0700

    Add grunt tools

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    },
+    wiredep: {
+      task: {
+        src: ['index.html']
+      },
+      options: {}
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-wiredep');
+
+  grunt.registerTask('default', ['jshint', 'wiredep']);
+
+};
